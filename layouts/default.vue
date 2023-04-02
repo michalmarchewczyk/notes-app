7705573ed98a97cb67a86f0db4ed4227315fd7fc
Michał Marchewczyk <dev@marchewczyk.eu>
2023-04-02 14:24:15 +0200

    Setup Nuxt 3 project

--- layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <NavBar />
-    <Nuxt class="pt-24 absolute block px-4 w-full" />
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    <h1 class="text-center text-5xl text-white font-light mt-16">
-      Marchewczyk.notes
-    </h1>
-  </div>
+  <h1>Index</h1>
 </template>
 
-<script>
-export default {
-  head: () => ({
-    title: 'Marchewczyk.notes'
-  })
-}
+<script setup>
+
 </script>
+
+<style scoped>
+
+</style>
