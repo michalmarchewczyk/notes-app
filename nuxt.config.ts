6eeda0b51432cc3d340d94f6d697e9d87cab884b
Michał Marchewczyk <dev@marchewczyk.eu>
2023-04-04 13:42:12 +0200

    Setup Firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,17 @@ export default defineNuxtConfig({
   build: {
     transpile: ["primevue"],
   },
+  modules: ["nuxt-vuefire"],
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+    admin: {
+      serviceAccount: "firebase-service-account-file.json",
+    },
+    auth: true,
+  },
 });
