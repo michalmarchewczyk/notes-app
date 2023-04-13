9f86a1c8060feeba1c6ec037955335025339abf8
Michał Marchewczyk <dev@marchewczyk.eu>
2023-04-14 00:48:16 +0200

    Add a placeholder for empty recent notes list

--- pages/app/notes/index.vue
+++ pages/app/notes/index.vue
@@ -15,7 +15,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>NOTES INDEX</div>
+  <div class="container">
+    <i class="ti ti-file"></i>
+    <span>Open note to start editing</span>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  gap: 2rem;
+  opacity: 0.5;
+  i {
+    font-size: 5rem;
+  }
+  span {
+    font-size: 1.6rem;
+    font-weight: 600;
+  }
+}
+</style>
